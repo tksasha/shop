78cf80a98b7ee14dbb7d7dd7de056d95ac55aeeb
Just Me <just806me@gmail.com>
2017-05-25 17:37:39 +0300

    add spec for user #resource_params

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  describe '#resource_params' do
+    let(:params) do
+      { user: { email: 'one@digits.com', password: 'password', password_confirmation: 'password' } }
+    end
+
+    before { expect(subject).to receive(:params).and_return(acp params) }
+
+    its(:resource_params) { should eq permit! params[:user] }
+  end
+
   it_behaves_like :new
 
   it_behaves_like :show do
@@ -9,6 +19,8 @@ RSpec.describe UsersController, type: :controller do
 
   it_behaves_like :create do
     let(:resource) { stub_model User }
+
+    before { expect(subject).to receive(:login_user) }
 
     let(:success) { -> { should redirect_to resource } }
 
