339393ee982025cc75fee3532d03208d24b6e6bc
Just Me <just806me@gmail.com>
2017-05-25 16:05:51 +0300

    add user specs

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+  it_behaves_like :new
+
+  it_behaves_like :create do
+    let(:resource) { stub_model User }
+
+    let(:success) { -> { should redirect_to resource } }
+
+    let(:faliture) { -> { should render_template :new } }
+  end
+end
