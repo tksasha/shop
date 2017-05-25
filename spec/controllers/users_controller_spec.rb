8092bc69b6802a73e653637f07ee8604d485fd48
Just Me <just806me@gmail.com>
2017-05-25 16:14:23 +0300

    add #show to user

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
   it_behaves_like :new
 
+  it_behaves_like :show do
+    before { expect(subject).to receive(:autorize_resource) }
+  end
+
   it_behaves_like :create do
     let(:resource) { stub_model User }
 
