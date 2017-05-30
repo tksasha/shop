d45291432f3ee48ec95d6c86cccfbe1ebc60a27d
Just Me <just806me@gmail.com>
2017-05-30 11:38:54 +0300

    add session

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+  after_action :login_user, only: :create
+
+  private
+  def resource
+    @resource ||= Session.new
+  end
+
+  def resource_params
+    params.require(:session).permit(:email, :password)
+  end
+
+  def login_user
+    session[:user_id] = resource.user_id if resource.persisted?
+  end
+end
---
+++ spec/policies/sessions_policy_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe SessionPolicy do
+  subject { described_class }
+
+  permissions :new?, :create? do
+    context do
+      let(:user) { nil }
+
+      let(:resource) { double }
+
+      it { should permit user, resource }
+    end
+
+    context do
+      let(:user) { User.new }
+
+      let(:resource) { double }
+
+      it { should_not permit user, resource }
+    end
+  end
+end
