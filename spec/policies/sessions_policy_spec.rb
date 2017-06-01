1e6ef67290844d5a1fe99832be78e3c2d24331ee
Just Me <just806me@gmail.com>
2017-06-01 22:20:50 +0300

    add policies for destroy

--- spec/policies/sessions_policy_spec.rb
+++ spec/policies/sessions_policy_spec.rb
@@ -20,4 +20,22 @@ describe SessionPolicy do
       it { should_not permit user, resource }
     end
   end
+
+  permissions :destroy? do
+    context do
+      let(:user) { nil }
+
+      let(:resource) { double }
+
+      it { should_not permit user, resource }
+    end
+
+    context do
+      let(:user) { User.new }
+
+      let(:resource) { double }
+
+      it { should permit user, resource }
+    end
+  end
 end
