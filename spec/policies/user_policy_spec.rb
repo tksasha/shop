fb2c4704bfa998b4b6354b7f92745bef4b3035e1
Just Me <just806me@gmail.com>
2017-06-03 17:31:55 +0300

    add user index specs

--- spec/policies/user_policy_spec.rb
+++ spec/policies/user_policy_spec.rb
@@ -46,4 +46,30 @@ describe UserPolicy do
       it { should permit user, resource }
     end
   end
+
+  permissions :index? do
+    context do
+      let(:user) { nil }
+
+      let(:resource) { double }
+
+      it { should_not permit user, resource }
+    end
+
+    context do
+      let(:user) { User.new }
+
+      let(:resource) { double }
+
+      it { should_not permit user, resource }
+    end
+
+    context do
+      let(:user) { User.new roles: [:admin] }
+
+      let(:resource) { double }
+
+      it { should permit user, resource }
+    end
+  end
 end
