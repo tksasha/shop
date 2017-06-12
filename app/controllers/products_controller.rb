41adf3de68afdff38e6ff819854734038a42eb2e
BerSlavik17 <tiras0507@gmail.com>
2017-06-12 18:08:00 +0300

    add eager loading; change _product.html.erb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,8 @@
 class ProductsController < ApplicationController
+  helper_method :eager_loaded_collection
+  
+  private
+  def eager_loaded_collection
+    @eager_loaded_collection ||= Product.includes(:categories) 
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ApplicationRecord
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  
   has_and_belongs_to_many :categories
   
-  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  paginates_per 10
 end
