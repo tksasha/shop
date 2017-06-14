b87b67f925e42a0ac4bbb2b39c9e6332944e4f9f
BerSlavik17 <tiras0507@gmail.com>
2017-06-14 18:13:14 +0300

    deleting helper method from products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,4 @@
 class ProductsController < ApplicationController
-  helper_method :collection
-
   private
   def collection
     @collection ||= Product.includes(:categories).order(:name).page params[:page]
