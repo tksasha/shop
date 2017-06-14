b474b42a136d448e7c39e3e4461948a14f170c55
BerSlavik17 <tiras0507@gmail.com>
2017-06-14 11:17:38 +0300

    add pagination for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,8 @@
 class ProductsController < ApplicationController
-  helper_method :collection, :products_for_paginating
+  helper_method :collection
 
   private
   def collection
-    @collection ||= Product.includes(:categories)
-  end
-
-  def products_for_paginating
-    @products_for_paginating ||= Product.order(:name).page params[:page]
+    @collection ||= Product.includes(:categories).order(:name).page params[:page]
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   validates :name, presence: true, uniqueness: { case_sensitive: false }
-  
+
   has_and_belongs_to_many :categories
-  
-  paginates_per 10
+
+  paginates_per 7
 end
