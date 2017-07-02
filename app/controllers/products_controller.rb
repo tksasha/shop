5c9f2ef9d88163cf26d778001bb4b5e101bd2a37
Just Me <just806me@gmail.com>
2017-07-02 19:10:39 +0300

    implement #2

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,4 @@
 class ProductsController < ApplicationController
-  include Rest
-
-  include Authorization
-
   private
   def collection
     @collection ||= Product.includes(:categories).order(:name).page params[:page]
