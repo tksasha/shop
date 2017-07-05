a28d15a640c8515c9cae28eceff1662e4fe619f4
Just Me <just806me@gmail.com>
2017-07-05 16:42:43 +0300

    add specs

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe ProductsController, type: :controller do
   it_behaves_like :index
 
   it_behaves_like :show
+
+  it_behaves_like :destroy, format: :js do
+    let(:success) { -> { should render_template :destroy } }
+  end
 end
