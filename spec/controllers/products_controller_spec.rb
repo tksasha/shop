b487dc1ced25a0b9d10f48f540cdb33731dcc4a2
Just Me <just806me@gmail.com>
2017-07-05 18:39:47 +0300

    use html for ProductsController#destroy

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe ProductsController, type: :controller do 
+RSpec.describe ProductsController, type: :controller do
   it_behaves_like :index
 
   it_behaves_like :show
+
+  it_behaves_like :destroy do
+    let(:success) { -> { should redirect_to :products } }
+  end
 
   it_behaves_like :destroy, format: :js do
     let(:success) { -> { should render_template :destroy } }
