cb35f2adf1698847a4c3e974016fcfd2d0fcf2e8
Alexander Tkachuk <mail@tksasha.me>
2017-07-19 18:20:58 +0300

    update db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -46,6 +46,13 @@ ActiveRecord::Schema.define(version: 20170718134509) do
     t.index ["deleted_at"], name: "index_products_on_deleted_at"
   end
 
+  create_table "products_tags", id: false, force: :cascade do |t|
+    t.integer "tag_id"
+    t.integer "product_id"
+    t.index ["product_id"], name: "index_products_tags_on_product_id"
+    t.index ["tag_id"], name: "index_products_tags_on_tag_id"
+  end
+
   create_table "purchases", force: :cascade do |t|
     t.integer "user_id"
     t.integer "product_id"
@@ -57,15 +64,21 @@ ActiveRecord::Schema.define(version: 20170718134509) do
     t.index ["user_id"], name: "index_purchases_on_user_id"
   end
 
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "roles"
+    t.datetime "blocked_at"
     t.boolean "confirmed", default: false, null: false
     t.string "confirmation_token"
-    t.datetime "blocked_at"
   end
 
 end
