e561a93008a8ced7c17fecd7d057b604b051425b
Alexander Tkachuk <mail@tksasha.me>
2017-08-16 11:27:26 +0300

    add script to mass upload Product and Category

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ end
 
 group :development do
   gem 'listen'
+  gem 'faker'
 end
 
 group :test do
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+raise 'Are you crazy?!!!' unless Rails.env.development?
+
+100.times.map do
+  Category.create name: Faker::Commerce.department
+end unless Category.first
+
+categories_count = Category.count
+
+5000.times.map do
+  offset = rand categories_count - 1
+
+  category = Category.offset(offset).first
+
+  Product.create name: Faker::Commerce.product_name, price: Faker::Commerce.price, categories: [category]
+end unless Product.first
