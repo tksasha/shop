95ea162df1fdd844c45ab833666162a9aef7cc60
Alexander Tkachuk <mail@tksasha.me>
2017-08-24 12:13:01 +0300

    refactores widgets for Categories; added `edit` button

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -1,8 +1,23 @@
 class WidgetsController < ApplicationController
   layout 'widgets'
 
+  helper_method :categories
+
   private
   def collection
     Widget
+  end
+
+  def categories
+    [
+      ['widgets/alcohol.jpg', 'Alcohol'],
+      ['widgets/sweets_and_desserts.jpg', 'Sweets and Desserts'],
+      ['widgets/beer.jpg', 'Beer'],
+      ['widgets/red_dry_wine.jpg', 'Red Dry Wine'],
+      ['widgets/stationery.jpg', 'Stationery'],
+      ['widgets/sausages_wieners_and_frankfurters.jpg', 'Sausages, Wieners and Frankfurters'],
+      ['widgets/donuts.jpg', 'Donuts'],
+      ['widgets/chickens.jpg', 'Chickens'],
+    ]
   end
 end
