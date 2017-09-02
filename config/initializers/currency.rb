72e7657f3c8f26e3a63543e08ccbad949acbfd34
Just Me <just806me@gmail.com>
2017-09-02 12:44:33 +0300

    update gems, use currency_options

--- config/initializers/currency.rb
+++ config/initializers/currency.rb
@@ -1,3 +1,7 @@
 Currency::Options.currencies = [:usd, :uah, :eur]
 
 Currency::Options.default_currency = :usd
+
+Currency::Options.currency_options[:usd] = { precision: 2, format: '$ %<amount>s' }
+
+Currency::Options.currency_options[:eur] = { precision: 2, format: '€ %<amount>s' }
