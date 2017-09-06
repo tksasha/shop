7c52cc595593461342530c82efebbb4177fe15e1
Just Me <just806me@gmail.com>
2017-09-06 18:13:19 +0300

    edit formating

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,15 +8,12 @@ class ProductsController < ApplicationController
   end
 
   private
-  #
-  # GET /products?page=...[&description=...][&name=...]
-  #
   def collection
-    @collection ||= ProductSearcher
-      .search(name: params[:name], description: params[:description])
-      .includes(:categories)
-      .order(:name)
-      .page params[:page]
+    @collection ||= ProductSearcher.
+      search(params).
+      includes(:categories).
+      order(:name).
+      page(params[:page])
   end
 
   def resource_params
