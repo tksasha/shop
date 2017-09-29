638e80669e4d211fe01144edcc531906f027bd3b
Alexander Tkachuk <mail@tksasha.me>
2017-09-29 14:45:38 +0300

    added seeds to fill database

--- app/models/category.rb
+++ app/models/category.rb
@@ -11,7 +11,7 @@ class Category < ApplicationRecord
 
   validates_attachment :image, presence: true,
     content_type: { content_type: ['image/jpeg', 'image/gif', 'image/png'] },
-    #file_name: { matches: [/jpe?g\z/, /gif\z/, /png\z/] },
+    file_name: { matches: [/jpe?g\z/, /gif\z/, /png\z/] },
     size: { in: 0..5.megabytes }
 
   pg_search_scope :search_by_name, against: :name, using: :trigram
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,33 @@
 raise 'Are you crazy?!!!' unless Rails.env.development?
 
 16.times.map do
-  Category.create! name: Faker::Commerce.product_name, image: Faker::LoremPixel.image('500x500')
+  filename = 'tmp/image.jpeg'
+
+  file = File.open filename, 'wb+'
+
+  file.write open(Faker::LoremPixel.image('500x500')).read
+
+  file.close
+
+  Category.create! name: Faker::Commerce.product_name, image: File.open(filename)
+
+  File.unlink filename
 end unless Category.first
 
-__END__
-categories_count = Category.count
-
-5000.times.map do
-  offset = rand categories_count - 1
-
-  category = Category.offset(offset).first
-
-  Product.create name: Faker::Commerce.product_name, price: Faker::Commerce.price, categories: [category]
-end unless Product.first
-
 Version.create unless Version.first
+
+Category.all.map do |category|
+  filename = 'tmp/image.jpeg'
+
+  file = File.open filename, 'wb+'
+
+  file.write open(Faker::LoremPixel.image('500x500')).read
+
+  file.close
+
+  Product.create! \
+    name: Faker::Commerce.product_name,
+    price: Faker::Commerce.price,
+    categories: [category],
+    image: File.open(filename)
+end unless Product.first
