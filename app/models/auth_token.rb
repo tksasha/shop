a9deb171714d76a28a6e6827bfcd1eaef7e711b6
Just Me <just806me@gmail.com>
2017-10-04 19:02:18 +0300

    add validations to auth_token

--- app/models/auth_token.rb
+++ app/models/auth_token.rb
@@ -1,3 +1,5 @@
 class AuthToken < ApplicationRecord
   belongs_to :user
+
+  validates :value, presence: true, uniqueness: { case_sensitive: false }
 end
--- spec/models/auth_token_spec.rb
+++ spec/models/auth_token_spec.rb
@@ -2,4 +2,8 @@ require 'rails_helper'
 
 RSpec.describe AuthToken, type: :model do
   it { should belong_to :user }
+
+  it { should validate_presence_of :value }
+
+  it { should validate_uniqueness_of(:value).case_insensitive }
 end
