b1cf427bc493fbdfb72350d4a7dbc26e6e43772b
Alexander Tkachuk <mail@tksasha.me>
2017-10-05 14:57:10 +0300

    change and use default values for PrettyPlacement; use Categories from database for widgets

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -4,20 +4,4 @@ class WidgetsController < ApplicationController
   skip_before_action :authorize_collection, only: :index
 
   layout 'widgets'
-
-  helper_method :categories
-
-  private
-  def categories
-    [
-      ['widgets/alcohol.jpg', 'Alcohol'],
-      ['widgets/sweets_and_desserts.jpg', 'Sweets and Desserts'],
-      ['widgets/beer.jpg', 'Beer'],
-      ['widgets/red_dry_wine.jpg', 'Red Dry Wine'],
-      ['widgets/stationery.jpg', 'Stationery'],
-      ['widgets/sausages_wieners_and_frankfurters.jpg', 'Sausages and Wieners'],
-      ['widgets/donuts.jpg', 'Donuts'],
-      ['widgets/chickens.jpg', 'Chickens'],
-    ]
-  end
 end
