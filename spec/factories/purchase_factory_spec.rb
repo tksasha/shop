a357dae3d37c111a8e2c12e4cd834fdbf35630a9
Just Me <just806me@gmail.com>
2017-10-18 16:42:49 +0300

    calculate purchase price by discount price if available

--- spec/factories/purchase_factory_spec.rb
+++ spec/factories/purchase_factory_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.describe PurchaseFactory do
   let(:product) { stub_model Product, price: 4 }
 
-  let(:params) { { user_id: 1, product_id: product.id, amount: 3, price: product.price * 3 } }
+  let(:params) { { user_id: 1, product_id: product.id, amount: 3, price: 12 } }
 
   subject { described_class.new params[:user_id], product_id: params[:product_id], amount: params[:amount] }
 
   describe '#params' do
-    before { expect(subject).to receive(:product).and_return(product) }
+    before { expect(subject).to receive(:price).and_return 12 }
 
     its(:params) { should eq params }
   end
@@ -20,8 +20,22 @@ RSpec.describe PurchaseFactory do
   end
 
   describe '#price' do
-    before { expect(subject).to receive(:product).and_return(product) }
+    before { expect(subject).to receive(:product_price).and_return(3) }
 
-    its(:price) { should eq 12 }
+    its(:price) { should eq 9 }
+  end
+
+  describe '#product_price' do
+    before { expect(subject).to receive(:product).and_return(product).twice }
+
+    context do
+      its(:product_price) { should eq 4 }
+    end
+
+    context do
+      let(:product) { stub_model Product, price: 4, discount_price: 2 }
+
+      its(:product_price) { should eq 2 }
+    end
   end
 end
