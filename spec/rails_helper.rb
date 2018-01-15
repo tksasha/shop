ce7b3040a8a444dfba2c98c4c8d8c156e781bf08
Just Me <just806me@gmail.com>
2018-01-15 15:55:25 +0200

    RenderTemplateWithStatusMatcher was added

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,17 +1,18 @@
 ENV["RAILS_ENV"] ||= 'test'
+
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'paperclip/matchers'
 require 'pundit/rspec'
 require 'aasm/rspec'
 
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 RSpec.configure do |config|
   config.mock_with :rspec
-  
+
   config.order = :random
-  
+
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
@@ -20,9 +21,9 @@ RSpec.configure do |config|
   config.include Paperclip::Shoulda::Matchers
 
   [:controller, :view, :request].each do |type|
-    config.include ::Rails::Controller::Testing::TestProcess, :type => type
-    config.include ::Rails::Controller::Testing::TemplateAssertions, :type => type
-    config.include ::Rails::Controller::Testing::Integration, :type => type
+    config.include ::Rails::Controller::Testing::TestProcess, type: type
+    config.include ::Rails::Controller::Testing::TemplateAssertions, type: type
+    config.include ::Rails::Controller::Testing::Integration, type: type
   end
 end
 
@@ -34,6 +35,4 @@ Shoulda::Matchers.configure do |config|
   end
 end
 
-# Checks for pending migrations before tests are run.
-# If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
