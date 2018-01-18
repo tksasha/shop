be859a2c7517dd31158d5d507919b2ae1eab554a
Just Me <just806me@gmail.com>
2018-01-18 18:47:48 +0200

    facebook session was refactored

--- spec/services/facebook/session_spec.rb
+++ spec/services/facebook/session_spec.rb
@@ -5,6 +5,30 @@ RSpec.describe Facebook::Session, type: :model do
 
   it { should validate_presence_of :access_token }
 
+  it { expect(subject.method(:save).original_name).to eq(:valid?) }
+
+  its(:persisted?) { should eq false }
+
+  describe '#auth_token' do
+    context do
+      before { subject.instance_variable_set :@auth_token, :auth_token }
+
+      its(:auth_token) { should eq :auth_token }
+    end
+
+    context do
+      before { expect(subject).to receive(:user).and_return(nil) }
+
+      its(:auth_token) { should be_nil }
+    end
+
+    context do
+      before { expect(subject).to receive_message_chain(:user, :auth_tokens, :create!).and_return(:auth_token) }
+
+      its(:auth_token) { should eq :auth_token }
+    end
+  end
+
   describe '#response' do
     context do
       let(:response) { double read: '{ "id": 1, "email": "one@digits.com" }' }
@@ -29,128 +53,51 @@ RSpec.describe Facebook::Session, type: :model do
     end
   end
 
-  describe '#save' do
-    context do
-      before { expect(subject).to receive(:valid?).and_return false }
-
-      before { expect(subject).to_not receive :create_auth_token }
-
-      its(:save) { should eq false }
-    end
-
-    context do
-      before { expect(subject).to receive(:valid?).and_return true }
-
-      context do
-        before { expect(subject).to receive(:create_auth_token).and_return nil }
-
-        its(:save) { should eq false }
-      end
-
-      context do
-        before do
-          #
-          # subject.create_auth_token.persisted? -> false
-          #
-          expect(subject).to receive(:create_auth_token) do
-            double.tap { |a| expect(a).to receive(:persisted?).and_return false }
-          end
-        end
-
-        its(:save) { should eq false }
-      end
-
-      context do
-        before do
-          #
-          # subject.create_auth_token.persisted? -> true
-          #
-          expect(subject).to receive(:create_auth_token) do
-            double.tap { |a| expect(a).to receive(:persisted?).and_return true }
-          end
-        end
-
-        its(:save) { should eq true }
-      end
-    end
-  end
-
-  describe '#persisted?' do
-    context do
-      before { subject.instance_variable_set :@persisted, true }
-
-      its(:persisted?) { should eq true }
-    end
-
-    context do
-      before { subject.instance_variable_set :@persisted, false }
-
-      its(:persisted?) { should eq false }
-    end
-
-    context do
-      before { subject.instance_variable_set :@persisted, nil }
-
-      its(:persisted?) { should eq false }
-    end
-  end
-
   describe '#access_token_must_be_valid' do
+    let(:call) { -> { subject.send :access_token_must_be_valid } }
+
     context do
-      before { expect(subject).to receive(:response).and_return({}) }
+      before { allow(subject).to receive(:response).and_return('id' => 1) }
 
-      before { subject.valid? }
-
-      it { expect(subject.errors[:access_token]).to include I18n.t('errors.messages.invalid') }
+      it { expect(&call).to_not change { subject.errors.details } }
     end
 
     context do
-      before { expect(subject).to receive(:response).and_return({ 'id' => 1 }) }
+      before { allow(subject).to receive(:response).and_return({}) }
 
-      before { subject.valid? }
+      it { expect(&call).to change { subject.errors.details }.to(access_token: [{ error: :invalid }]) }
+    end
 
-      it { expect(subject.errors[:access_token]).to_not include I18n.t('errors.messages.invalid') }
+    context do
+      before { allow(subject).to receive(:response).and_return('id' => nil) }
+
+      it { expect(&call).to change { subject.errors.details }.to(access_token: [{ error: :invalid }]) }
     end
   end
 
   describe '#user' do
-    let(:user) { double }
-
-    before { expect(subject).to receive(:response).and_return({ 'id' => 1, 'email' => 'one@digits.com' }).twice }
-
-    before { expect(User).to receive(:find_or_create_by).with(facebook_id: 1).and_yield(user).and_return :user }
-
-    before { expect(user).to receive(:email=).with 'one@digits.com' }
-
-    its(:user) { should eq :user }
-  end
-
-  describe '#create_auth_token' do
     context do
       let(:user) { double }
 
-      before { expect(subject).to receive(:auth_token).and_return :auth_token }
+      before { allow(subject).to receive(:response).and_return({ 'id' => 1, 'email' => 'one@digits.com' }) }
 
-      before { expect(subject).to receive(:user).and_return(user).twice }
+      before { expect(User).to receive(:find_or_create_by).with(facebook_id: 1).and_yield(user).and_return(:user) }
 
-      before do
-        #
-        # user.auth_tokens.create value: :auth_token -> :create_auth_token
-        #
-        expect(user).to receive(:auth_tokens) do
-          double.tap do |a|
-            expect(a).to receive(:create).with(value: :auth_token).and_return :create_auth_token
-          end
-        end
-      end
+      before { expect(user).to receive(:email=).with('one@digits.com') }
 
-      its(:create_auth_token) { should eq :create_auth_token }
+      its(:user) { should eq :user }
     end
 
     context do
-      before { expect(subject).to receive(:user).and_return nil }
+      before { allow(subject).to receive(:response).and_return({}) }
 
-      its(:create_auth_token) { should be_nil }
+      its(:user) { should be_nil }
+    end
+
+    context do
+      before { allow(subject).to receive(:response).and_return('id' => nil) }
+
+      its(:user) { should be_nil }
     end
   end
 end
